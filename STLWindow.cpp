4beeb6ef03c9f174d601be7a42f1a244583a46d6
Gerasim Troeglazov <3dEyes@gmail.com>
2020-10-30 21:52:12 +1000

    Add GPLv2 to about window

--- STLWindow.cpp
+++ STLWindow.cpp
@@ -193,9 +193,20 @@ STLWindow::MessageReceived(BMessage *message)
 			break;
 		case B_ABOUT_REQUESTED:
 		{
-			BAboutWindow* wind = new BAboutWindow("STLover", APP_SIGNATURE);
+			BAboutWindow* wind = new BAboutWindow("STLover - Simple STL Viewer", APP_SIGNATURE);
 			wind->AddCopyright(2020, "Gerasim Troeglazov (3dEyes**)");
-			wind->AddDescription("Simple STL Viewer");
+			wind->AddDescription(
+				"This program is free software; you can redistribute it and/or modify "
+				"it under the terms of the GNU General Public License as published by "
+				"the Free Software Foundation; either version 2 of the License, or "
+				"(at your option) any later version.\n\n"
+				"This program is distributed in the hope that it will be useful, "
+ 				"but WITHOUT ANY WARRANTY; without even the implied warranty of "
+ 				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+ 				"GNU General Public License for more details.\n\n"
+				"You should have received a copy of the GNU General Public License along"
+ 				"with this program; if not, write to the Free Software Foundation, Inc., "
+ 				"51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA." );
 			wind->Show();
 			break;
 		}
